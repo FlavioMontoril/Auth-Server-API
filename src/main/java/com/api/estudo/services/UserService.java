8191f44d9778c9f960583f40809cdc90aa201bed
FlavioMontoril <flavio.montorilll@gmail.com>
2026-05-31 07:55:20 -0300

    created method of paginatation and method of get all users with pagination

--- src/main/java/com/api/estudo/services/UserService.java
+++ src/main/java/com/api/estudo/services/UserService.java
@@ -3,9 +3,13 @@ package com.api.estudo.services;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.api.estudo.domain.dtos.common.PageResponseDTO;
 import com.api.estudo.domain.dtos.user.UserRequestDTO;
 import com.api.estudo.domain.dtos.user.UserResponseDTO;
 import com.api.estudo.domain.entities.User;
@@ -41,6 +45,28 @@ public class UserService {
         user.setPassword(encryptedPassword);
 
         this.userRepository.save(user);
+    }
+
+    public PageResponseDTO<UserResponseDTO> findAllPagination(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        // EXPRESSAO LAMBDA
+        // Page<UserResponseDTO> users = this.userRepository.findAll(pageable).map(user
+        // -> new UserResponseDTO(
+        // user.getId(),
+        // user.getName(),
+        // user.getEmail(),
+        // user.getCreatedAt()));
+
+        // FORMA REDUZIDA DA EXPRESSAO LAMBDA
+        Page<UserResponseDTO> users = this.userRepository.findAll(pageable).map(UserResponseDTO::new);
+
+        return new PageResponseDTO<>(
+                users.getContent(),
+                users.getNumber(),
+                users.getSize(),
+                users.getTotalPages(),
+                users.getTotalElements());
     }
 
     public UserResponseDTO findById(UUID userId) {
