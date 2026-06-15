b46e88deda2f1ff5cf66a73ad90d72f28d9f2ae6
FlavioMontoril <flavio.montorilll@gmail.com>
2026-06-14 22:04:02 -0300

    refactor: ajuste geral

--- src/test/java/com/api/authserver/AuthServerApplicationTests.java
+++ src/test/java/com/api/authserver/AuthServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.api.authserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local") // Força o uso do application-local.properties
 class AuthServerApplicationTests {
 
 	@Test
